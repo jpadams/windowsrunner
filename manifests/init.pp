9bc26dd466e230a52afc89f178a5789bc95d5699
Jeremy Adams <jeremy@puppetlabs.com>
2013-11-13 21:58:23 -0700

    Improved installation in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,29 +2,6 @@
 
 class windowsrunner {
 
-  # For the Puppet Enterprise Master server
-  if $::kernel == 'Linux' {
-
-    file { '/opt/puppet/libexec/mcollective/mcollective/agent/windowsrunner.ddl':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      mode   => '0644',
-      source => 'puppet:///modules/windowsrunner/windowsrunner.ddl',
-      notify => Service['pe-mcollective'],
-    }
-
-    file { '/opt/puppet/libexec/mcollective/mcollective/agent/windowsrunner.rb':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      mode   => '0644',
-      source => 'puppet:///modules/windowsrunner/windowsrunner.rb',
-      notify => Service['pe-mcollective'],
-    }
-
-  }
-
   if $::kernel == 'windows' {
 
     file { 'C:\ProgramData\PuppetLabs\mcollective\etc\plugins\mcollective\agent\windowsrunner.ddl':
@@ -47,4 +24,33 @@ class windowsrunner {
 
   }
 
-}  
+  # For the Puppet Enterprise Master server
+  elsif $::kernel == 'Linux' {
+
+    file { '/opt/puppet/libexec/mcollective/mcollective/agent/windowsrunner.ddl':
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/windowsrunner/windowsrunner.ddl',
+      notify => Service['pe-mcollective'],
+    }
+
+    file { '/opt/puppet/libexec/mcollective/mcollective/agent/windowsrunner.rb':
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/windowsrunner/windowsrunner.rb',
+      notify => Service['pe-mcollective'],
+    }
+
+  }
+
+  else {
+
+    notify { 'Windowsrunner only supported on Linux master and Windows agent nodes': }
+
+  }
+
+}
